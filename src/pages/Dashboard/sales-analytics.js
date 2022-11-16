3eb281878c294e96a7a01c2bba612b515aa53dba
Asiqur Rahman Khan <asiqur.rahman@datasoft-bd.com>
2022-11-16 15:47:47 +0600

    Update

--- src/pages/Dashboard/sales-analytics.js
+++ src/pages/Dashboard/sales-analytics.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { Row, Col, Card, CardBody } from "reactstrap"
+import { Input, Row, Col, Card, CardBody } from "reactstrap"
 import ReactApexChart from "react-apexcharts"
 import TableLoader from "../../components/Common/TableLoader"
 import Axios from "../../helpers/axios_helper"
@@ -9,13 +9,19 @@ const ExpenseAnalysis = ( props ) => {
   const [analysis, setAnalysis] = useState(false);
 
   const loadData = async (days=7) =>{
-      await Axios.get(`/dashboard/expenseAccountReview`)
+      await Axios.get(`/dashboard/expenseAccountReview/${days}`)
           .then((response) => { 
-              const details = response.data;
+              const details = response.data[0];
+              const series = details?.amount?details.amount.split(','):[]
+              const labels = details?.names?details.names.split(','):[]
+              const cSeries =[]
+              series.forEach(element => {
+                cSeries.push(parseFloat(element))
+              });
               setAnalysis({
-                series: details.value,//[38, 26, 14],
+                series: cSeries,//[38, 26, 14],
                 options: {
-                  labels: details.key,//["Online", "Offline", "Marketing"],
+                  labels: labels,//["Online", "Offline", "Marketing"],
                   plotOptions: {
                     pie: {
                       donut: {
@@ -40,6 +46,17 @@ const ExpenseAnalysis = ( props ) => {
       <>
         <Card>
           <CardBody>
+            <div className="float-end">
+                <div className="input-group">
+                    <label className="input-group-text" style={{marginTop:'0'}}>Sort By</label>
+                    <Input type="select" className="form-select form-select-sm" onChange={(e)=>{loadData(e.target.value)}}>
+                        <option value="7">7 Days</option>
+                        <option value="30">30 Days</option>
+                        <option value="60">60 Days</option>
+                        <option value="90">90 Days</option>
+                    </Input>
+                </div>
+            </div>
             <h4 className="card-title mb-4">Expense Overview</h4>
 
             <Row className="align-items-center" style={{minHeight:analysis?"260px":"0px"}}>
